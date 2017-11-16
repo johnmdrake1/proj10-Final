f931485b9d5c90cbca48a596b960733fe04782b2
John Drake <jdrake@uoregon.edu>
2017-11-16 01:33:28 -0800

    Working commit
    Everything should be working properly, pending additional testing and possible cleanup of code and webpage layout.

--- meetings/flask_main.py
+++ meetings/flask_main.py
@@ -84,6 +84,7 @@ def choose2():
 
     gcal_service = get_gcal_service(credentials)
     app.logger.debug("Returned from get_gcal_service")
+    flask.g.calendars = list_calendars(gcal_service)
     flask.g.events = get_events(gcal_service)
     #list_events(gcal_service)
     return render_template('index.html')
---
+++ meetings/test_check.py
@@ -0,0 +1,3 @@
+import nose
+from flask_main import cmp_times as cmps
+
