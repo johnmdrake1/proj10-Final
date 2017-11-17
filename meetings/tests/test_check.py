ad77a4cbecfde456cbc6124f6e516f05b13fdee5
John Drake <jdrake@uoregon.edu>
2017-11-16 20:12:47 -0800

    submit and tests
    submit button and tests, one of the final submissions for all intents and purposes

--- meetings/tests/test_check.py
+++ meetings/tests/test_check.py
@@ -12,7 +12,21 @@ def test_empty():
 
 
 # 2.transparency
+def test_trans():
+	events = [{"sum": "test event", "transparency": " ", "start":"2017-01-01T00:00:00+00:00", "end": "2017-02-01T00:00:00+00:00"}]
+	start = "2017-01-01T00:00:00+00:00"
+	end = "2017-02-01T00:00:00+00:00"
+	result = cmps(events, start, end)
+	assert result=="no events"
 
 
 # 3.checking if there is event in a space where it should be
+
+def test_event():
+	events = [{"sum": "test event", "start": "2017-01-01T00:00:00+00:00", "end": "2017-03-01T11:59:00+00:00"}]
+	st = "2017-01-01T00:00:00+00:00"
+	et = "2017-05-05T11:59:00+00:00"
+	result = cmps(events, st, et)
+	#this one might not be working, remove for other tests
+	assert result not empty or (result!="no events")
 
